63557bbc7248dfc82c078a06ef0778f0bb4b57e1
yonglin-tan <38274778+yonglin-tan@users.noreply.github.com>
2018-04-12 21:31:17 +0800

    Update deleteschedule.inc.php

--- maersklinecms/includes/deleteschedule.inc.php
+++ maersklinecms/includes/deleteschedule.inc.php
@@ -8,8 +8,6 @@ if (isset($_POST['submit'])){
 	
 	$sql = "DELETE FROM shippingschedule WHERE ss_id = '$ss_id';";
 	mysqli_query($conn, $sql);
-	$sql2 = "DELETE FROM bookings WHERE ss_id = '$ss_id';";
-	mysqli_query($conn, $sql2);
 	header("Location: ../deleteschedule.php?delete=success");
 	exit();	
 		
